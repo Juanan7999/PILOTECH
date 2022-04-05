533e7d8a1fc00fb201877a33cc7b445785b44ce7
Juanan7999 <juanan7999@uma.es>
2022-04-05 17:31:29 +0200

    Corrección Unidireccionalidad Divisa
    Se ha modificado la relación Divisa-Transacción y Divisa-Cuenta_Referenciada para que sea unidireccional.

--- proyecto/proyecto-jpa/src/main/java/es/uma/proyecto/Divisa.java
+++ proyecto/proyecto-jpa/src/main/java/es/uma/proyecto/Divisa.java
@@ -2,7 +2,6 @@ package es.uma.proyecto;
 
 import java.io.Serializable;
 import javax.persistence.*;
-import java.util.List;
 import java.util.Objects;
 
 
@@ -25,18 +24,6 @@ public class Divisa implements Serializable {
 	@Column(nullable = false)
 	private Integer cambioeuro;
 	
-	//bi-directional many-to-one association to CuentaReferencia
-	@OneToMany(mappedBy="divisa")
-	private List<CuentaReferencia> cuentaReferencias;
-
-	//bi-directional many-to-one association to Transaccion
-	@OneToMany(mappedBy="divisa1")
-	private List<Transaccion> transaccions1;
-
-	//bi-directional many-to-one association to Transaccion
-	@OneToMany(mappedBy="divisa2")
-	private List<Transaccion> transaccions2;
-
 	public Divisa() {
 	}
 
@@ -72,72 +59,6 @@ public class Divisa implements Serializable {
 		this.simbolo = simbolo;
 	}
 
-	public List<CuentaReferencia> getCuentaReferencias() {
-		return this.cuentaReferencias;
-	}
-
-	public void setCuentaReferencias(List<CuentaReferencia> cuentaReferencias) {
-		this.cuentaReferencias = cuentaReferencias;
-	}
-
-	public CuentaReferencia addCuentaReferencia(CuentaReferencia cuentaReferencia) {
-		getCuentaReferencias().add(cuentaReferencia);
-		cuentaReferencia.setDivisa(this);
-
-		return cuentaReferencia;
-	}
-
-	public CuentaReferencia removeCuentaReferencia(CuentaReferencia cuentaReferencia) {
-		getCuentaReferencias().remove(cuentaReferencia);
-		cuentaReferencia.setDivisa(null);
-
-		return cuentaReferencia;
-	}
-
-	public List<Transaccion> getTransaccions1() {
-		return this.transaccions1;
-	}
-
-	public void setTransaccions1(List<Transaccion> transaccions1) {
-		this.transaccions1 = transaccions1;
-	}
-
-	public Transaccion addTransaccions1(Transaccion transaccions1) {
-		getTransaccions1().add(transaccions1);
-		transaccions1.setDivisa1(this);
-
-		return transaccions1;
-	}
-
-	public Transaccion removeTransaccions1(Transaccion transaccions1) {
-		getTransaccions1().remove(transaccions1);
-		transaccions1.setDivisa1(null);
-
-		return transaccions1;
-	}
-
-	public List<Transaccion> getTransaccions2() {
-		return this.transaccions2;
-	}
-
-	public void setTransaccions2(List<Transaccion> transaccions2) {
-		this.transaccions2 = transaccions2;
-	}
-
-	public Transaccion addTransaccions2(Transaccion transaccions2) {
-		getTransaccions2().add(transaccions2);
-		transaccions2.setDivisa2(this);
-
-		return transaccions2;
-	}
-
-	public Transaccion removeTransaccions2(Transaccion transaccions2) {
-		getTransaccions2().remove(transaccions2);
-		transaccions2.setDivisa2(null);
-
-		return transaccions2;
-	}
-
 	@Override
 	public int hashCode() {
 		return Objects.hash(abreviatura);
@@ -159,7 +80,4 @@ public class Divisa implements Serializable {
 	public String toString() {
 		return "Divisa [nombre=" + nombre + ", abreviatura=" + abreviatura + ", cambioeuro=" + cambioeuro + "]";
 	}
-	
-	
-	
 }
