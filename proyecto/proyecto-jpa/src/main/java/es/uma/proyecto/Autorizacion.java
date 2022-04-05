3cba3b9e1f4ddfad1fa5097fe2a2a2b531bf546d
Juanan7999 <juanan7999@uma.es>
2022-04-05 18:49:28 +0200

    Revert "Corrección Error Relaciones DDL"
    This reverts commit 9e225cf29432d0bc4e1e623ce95b8cb32ab02857.

--- proyecto/proyecto-jpa/src/main/java/es/uma/proyecto/Autorizacion.java
+++ proyecto/proyecto-jpa/src/main/java/es/uma/proyecto/Autorizacion.java
@@ -22,12 +22,12 @@ public class Autorizacion implements Serializable {
 
 	//bi-directional many-to-one association to Empresa
 	@ManyToOne
-	@JoinColumn(nullable = false)
+	@JoinColumn(name="AUTORIZACION_EMPRESA", nullable = false)
 	private Empresa empresa;
 
 	//bi-directional many-to-one association to PersonaAutorizada
 	@ManyToOne
-	@JoinColumn(nullable = false)
+	@JoinColumn(name="PERSONA_AUTORIZADA_ID", nullable = false)
 	private PersonaAutorizada personaAutorizada;
 
 	public Autorizacion() {
