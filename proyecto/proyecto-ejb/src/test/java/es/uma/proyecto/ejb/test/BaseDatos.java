ad253fc3dd5100afe2f7e873305de3fb248df2f0
jcarlosgarciaa <80254558+jcarlosgarciaa@users.noreply.github.com>
2022-04-26 18:49:47 +0200

    Primera parte pruebas
    Primera parte pruebas de Cliente

--- proyecto/proyecto-ejb/src/test/java/es/uma/proyecto/ejb/test/BaseDatos.java
+++ proyecto/proyecto-ejb/src/test/java/es/uma/proyecto/ejb/test/BaseDatos.java
@@ -1,7 +1,8 @@
 package es.uma.proyecto.ejb.test;
 
 import java.math.BigDecimal;
-import java.sql.Date;
+
+import java.util.Date;
 import java.util.HashMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -10,6 +11,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import es.uma.proyecto.Empresa;
+import es.uma.proyecto.Individual;
 import es.uma.proyecto.Usuario;
 
 /*
@@ -31,6 +34,67 @@ public class BaseDatos {
 		usuario_administrativo.setTipo("A");
 		
 		em.persist(usuario_administrativo);
+		
+		Individual nuevo_clientebaja = new Individual();
+		nuevo_clientebaja.setIdentificacion("77670010");
+		nuevo_clientebaja.setTipoCliente("F");
+		nuevo_clientebaja.setEstado("baja");
+		nuevo_clientebaja.setFechaAlta(new Date("2022-04-23"));
+		
+		
+		nuevo_clientebaja.setFechaBaja(null);
+		
+		
+		nuevo_clientebaja.setDireccion("Calle Chozuelas");
+		nuevo_clientebaja.setCiudad("Alora");
+		nuevo_clientebaja.setCodigopostal(29500);
+		nuevo_clientebaja.setPais("España");
+		
+		nuevo_clientebaja.setNombre("Pepe");
+		nuevo_clientebaja.setApellido("Acedo");
+		nuevo_clientebaja.setFechaNacimiento(null);
+		em.persist(nuevo_clientebaja);
+		
+		Individual nuevo_cliente = new Individual();
+		nuevo_cliente.setIdentificacion("77670018");
+		nuevo_cliente.setTipoCliente("F");
+		nuevo_cliente.setEstado("activo");
+		nuevo_cliente.setFechaAlta(new Date("2022-04-23"));
+		
+		
+		nuevo_cliente.setFechaBaja(null);
+		
+		
+		nuevo_cliente.setDireccion("Calle Chozuelas");
+		nuevo_cliente.setCiudad("Alora");
+		nuevo_cliente.setCodigopostal(29500);
+		nuevo_cliente.setPais("España");
+		
+		nuevo_cliente.setNombre("Jose");
+		nuevo_cliente.setApellido("Garcia");
+		nuevo_cliente.setFechaNacimiento(null);
+		em.persist(nuevo_cliente);
+		
+		
+		Empresa nueva_empresa = new Empresa();
+		nueva_empresa.setIdentificacion("8888");
+		nueva_empresa.setTipoCliente("J");
+		nueva_empresa.setEstado("activo");
+		nueva_empresa.setFechaAlta(new Date("2022-04-23"));
+		
+		
+			nueva_empresa.setFechaBaja(null);
+		
+		
+		nueva_empresa.setDireccion("Boulevard Pasteur");
+		nueva_empresa.setCiudad("Malaga");
+		nueva_empresa.setCodigopostal(29010);
+		nueva_empresa.setPais("España");
+		
+		nueva_empresa.setRazonSocial("Pilotech");
+		
+		
+		
 		
 		em.getTransaction().commit();
 		
--- proyecto/proyecto-ejb/src/test/java/es/uma/proyecto/ejb/test/ClientePr.java
+++ proyecto/proyecto-ejb/src/test/java/es/uma/proyecto/ejb/test/ClientePr.java
@@ -1,5 +1,169 @@
 package es.uma.proyecto.ejb.test;
 
-public class ClientePr {
+import org.junit.*;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.naming.NamingException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import es.uma.informatica.sii.anotaciones.Requisitos;
+import es.uma.proyecto.Cliente;
+import es.uma.proyecto.Individual;
+import es.uma.proyecto.ejb.GestionCliente;
+import es.uma.proyecto.ejb.GestionUsuario;
+import es.uma.proyecto.ejb.exceptions.ClienteExistenteException;
+import es.uma.proyecto.ejb.exceptions.ClienteNoExistenteException;
+import es.uma.proyecto.ejb.exceptions.ClienteYaDeBajaException;
+import es.uma.proyecto.ejb.exceptions.CuentaAbiertaException;
+import es.uma.proyecto.ejb.exceptions.ProyectoEjbException;
+import es.uma.proyecto.ejb.exceptions.UsuarioNoEsAdministrativoException;
+
+public class ClientePr {
+	private static final Logger LOG = Logger.getLogger(ClientePr.class.getCanonicalName());
+
+	private static final String CLIENTE_EJB = "java:global/classes/ClienteEJB";
+	private static final String UNIDAD_PERSITENCIA_PRUEBAS = "proyecto-ejbTest";
+	
+	private GestionCliente gestionCliente;
+	
+	@Before
+	public void setup() throws NamingException  {
+		gestionCliente = (GestionCliente) SuiteTest.ctx.lookup(CLIENTE_EJB);
+		BaseDatos.inicializaBaseDatos(UNIDAD_PERSITENCIA_PRUEBAS);
+	}
+	
+	@Requisitos({"RF2"})
+	@Test
+	public void testAltaClienteIndividual() throws ClienteExistenteException, UsuarioNoEsAdministrativoException {
+		List<Cliente> clientes1 = gestionCliente.devolverTodosClientes();
+		try {
+		gestionCliente.altaClienteIndividual("Juan", "77670017", "F", "activo", new Date("2022-04-23"), null, "Calle Chozuelas" ,"Alora",  29500, "España", "Juanito", "Lopez", null);
+		}catch(ProyectoEjbException e) {
+		fail("Excepcion inesperada");	
+		}
+		List<Cliente> clientes2 = gestionCliente.devolverTodosClientes();
+		assertEquals(clientes2.size(), clientes1.size()+1);
+		
+	}
+	
+	
+	@Requisitos({"RF2"})
+	@Test
+	public void testAltaClienteIndividualExistente() throws ClienteExistenteException, UsuarioNoEsAdministrativoException {
+		
+		try {
+		gestionCliente.altaClienteIndividual("Juan", "77670018", "F", "activo", new Date("2022-04-23"), null, "Calle Chozuelas" ,"Alora",  29500, "España", "Jose", "Garcia", null);
+		fail("Deberia haber saltado la excepcion porque es un cliete existente");
+		}catch(ClienteExistenteException e) {
+		//OK	
+		}catch(ProyectoEjbException e) {
+		fail("Excepcion inesperada");	
+		}
+		
+		
+	}
+	
+	@Requisitos({"RF2"})
+	@Test
+	public void testAltaClienteIndividualConNoAdmin() throws ClienteExistenteException, UsuarioNoEsAdministrativoException {
+		
+		try {
+		gestionCliente.altaClienteIndividual("Juan1", "77670018", "F", "activo", new Date("2022-04-23"), null, "Calle Chozuelas" ,"Alora",  29500, "España", "Jose", "Garcia", null);
+		fail("Deberia haber saltado la excepcion porque no es un administrador");
+		}catch(UsuarioNoEsAdministrativoException e) {
+		//OK	
+		}catch(ProyectoEjbException e) {
+		fail("Excepcion inesperada");	
+		}
+		
+		
+	}
+	
+	@Requisitos({"RF2"})
+	@Test
+	public void testAltaClienteEmpresa() throws ClienteExistenteException, UsuarioNoEsAdministrativoException {
+		List<Cliente> clientes1 = gestionCliente.devolverTodosClientes();
+		try {
+		gestionCliente.altaClienteEmpresa("Juan", "8885", "J", "activo", new Date("2022-04-23"), null, "Calle Chozuelas" ,"Alora",  29500, "España", "Burger King");
+		}catch(ProyectoEjbException e) {
+		fail("Excepcion inesperada");	
+		}
+		List<Cliente> clientes2 = gestionCliente.devolverTodosClientes();
+		assertEquals(clientes2.size(), clientes1.size()+1);
+		
+	}
+	
+	@Requisitos({"RF2"})
+	@Test
+	public void testAltaClienteEmpresaExistente() throws ClienteExistenteException, UsuarioNoEsAdministrativoException {
+		
+		try {
+			gestionCliente.altaClienteEmpresa("Juan", "8888", "J", "activo", new Date("2022-04-23"), null, "Boulevard Pasteur" ,"Malaga",  29010, "España", "Pilotech");
+		fail("Deberia haber saltado la excepcion porque es un cliete existente");
+		}catch(ClienteExistenteException e) {
+		//OK	
+		}catch(ProyectoEjbException e) {
+		fail("Excepcion inesperada");	
+		}
+		
+		
+	}
+	
+	@Requisitos({"RF2"})
+	@Test
+	public void testAltaClienteEmpresaConNoAdmin() throws ClienteExistenteException, UsuarioNoEsAdministrativoException {
+		
+		try {
+			gestionCliente.altaClienteEmpresa("Juan1", "8888", "J", "activo", new Date("2022-04-23"), null, "Boulevard Pasteur" ,"Malaga",  29010, "España", "Pilotech");
+		fail("Deberia haber saltado la excepcion porque el que lo introduce no es un admin");
+		}catch(UsuarioNoEsAdministrativoException e) {
+		//OK	
+		}catch(ProyectoEjbException e) {
+		fail("Excepcion inesperada");	
+		}
+		
+		
+	}
+	
+	@Requisitos({"RF3"})
+	@Test
+	public void testBajaClienteNoExistente() throws ClienteYaDeBajaException, CuentaAbiertaException, UsuarioNoEsAdministrativoException {
+		
+		try {
+			
+			
+			gestionCliente.bajaCliente("Juan", "77670018");
+			}catch(ClienteNoExistenteException e) {
+			fail("Este cliente no existe");	
+			}catch(ProyectoEjbException e) {
+				fail("Excepcion inesperada");	
+			}
+	}
+	
+	@Requisitos({"RF3"})
+	@Test
+	public void testBajaClienteYaDeBaja() throws ClienteYaDeBajaException, CuentaAbiertaException, UsuarioNoEsAdministrativoException {
+		
+		try {
+			
+			gestionCliente.bajaCliente("Juan", "77670010");
+			fail("El cliente ya estaba de baja");	
+			}catch(ClienteYaDeBajaException e) {
+			//OK
+			}catch(ProyectoEjbException e) {
+				fail("Excepcion inesperada");	
+			}
+	}
+	
+	
+	
+	
 }
