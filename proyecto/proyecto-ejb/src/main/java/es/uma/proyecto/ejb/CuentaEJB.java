5e0787af1433e38a116071024076e54eee768619
Juanan7999 <juanan7999@uma.es>
2022-04-26 20:37:01 +0200

    GestionCuentas + Implementeción Metodos

--- proyecto/proyecto-ejb/src/main/java/es/uma/proyecto/ejb/CuentaEJB.java
+++ proyecto/proyecto-ejb/src/main/java/es/uma/proyecto/ejb/CuentaEJB.java
@@ -1,18 +1,180 @@
 package es.uma.proyecto.ejb;
 
+import java.util.Date;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import es.uma.proyecto.Autorizacion;
+import es.uma.proyecto.AutorizacionPK;
+import es.uma.proyecto.Cliente;
+import es.uma.proyecto.CuentaReferencia;
+import es.uma.proyecto.DepositaEn;
+import es.uma.proyecto.Empresa;
+import es.uma.proyecto.PersonaAutorizada;
+import es.uma.proyecto.PooledAccount;
+import es.uma.proyecto.Segregada;
+import es.uma.proyecto.Usuario;
+import es.uma.proyecto.ejb.exceptions.ClienteNoJuridicoException;
+import es.uma.proyecto.ejb.exceptions.PersonaAutorizadaNoExistenteException;
+import es.uma.proyecto.ejb.exceptions.PooledAccountConSolo1CuentaExternaException;
+import es.uma.proyecto.ejb.exceptions.UsuarioNoEsAdministrativoException;
+
 public class CuentaEJB implements GestionCuenta{
 
-private static final Logger LOG = Logger.getLogger(ClienteEJB.class.getCanonicalName());
+private static final Logger LOG = Logger.getLogger(CuentaEJB.class.getCanonicalName());
 	
 	@PersistenceContext(name="proyecto-ejb")
 	private EntityManager em;
-	
-	
-	
-	
+
+	@Override
+	public void abrirCuentaFintechSegregada(String idAdm, String iban, String swift, String estado,
+			Date fecha_apertura, Date fecha_cierre, String clasificacion, String comision, CuentaReferencia cr) throws UsuarioNoEsAdministrativoException {
+		
+		Usuario administrador = em.find(Usuario.class, idAdm);
+		
+		if(administrador==null || !administrador.getTipo().equals("A")) { //Si no existe o no es administrativo
+			throw new UsuarioNoEsAdministrativoException();
+		}
+		
+		Segregada cuentaSegregada = new Segregada();
+		
+		cuentaSegregada.setIban(iban);
+		cuentaSegregada.setSwift(swift);
+		cuentaSegregada.setEstado(estado);
+		cuentaSegregada.setFechaApertura(fecha_apertura);
+		
+		if(fecha_cierre !=null) {
+			cuentaSegregada.setFechaCierre(fecha_cierre);
+		}
+		
+		if(clasificacion != null) {
+			cuentaSegregada.setClasificacion(clasificacion);
+		}
+		
+		if(comision != null) {
+			cuentaSegregada.setComision(comision);
+		}
+		
+		cuentaSegregada.setCuentaReferencia(cr);
+		em.persist(cuentaSegregada);
+	}
+
+	@Override
+	public void abrirCuentaFintechPooled(String idAdm, String iban, String swift, String estado, Date fecha_apertura,
+			Date fecha_cierre, String clasificacion, List<DepositaEn> lcr) throws UsuarioNoEsAdministrativoException, PooledAccountConSolo1CuentaExternaException {
+		
+		Usuario administrador = em.find(Usuario.class, idAdm);
+		
+		if(administrador==null || !administrador.getTipo().equals("A")) { //Si no existe o no es administrativo
+			throw new UsuarioNoEsAdministrativoException();
+		}
+		
+		PooledAccount cuentaPooled = new PooledAccount();
+		
+		cuentaPooled.setIban(iban);
+		cuentaPooled.setSwift(swift);
+		cuentaPooled.setEstado(estado);
+		cuentaPooled.setFechaApertura(fecha_apertura);
+		
+		if(fecha_cierre !=null) {
+			cuentaPooled.setFechaCierre(fecha_cierre);
+		}
+		
+		if(clasificacion != null) {
+			cuentaPooled.setClasificacion(clasificacion);
+		}
+		
+		if(lcr.size()<2) {
+			throw new PooledAccountConSolo1CuentaExternaException();
+		}
+		
+		cuentaPooled.setDepositaEns(lcr);
+		em.persist(cuentaPooled);
+		
+	}
+
+	@Override
+	public void anadirAutorizados(String idAdm, List<PersonaAutorizada> lpa, Cliente c) throws UsuarioNoEsAdministrativoException, ClienteNoJuridicoException {
+		
+		Usuario administrador = em.find(Usuario.class, idAdm);
+		
+		if(administrador==null || !administrador.getTipo().equals("A")) { //Si no existe o no es administrativo
+			throw new UsuarioNoEsAdministrativoException();
+		}
+		
+		if(!c.getEstado().equals("J")) {
+			throw new ClienteNoJuridicoException();
+		}
+		
+		Empresa e = em.find(Empresa.class, c.getId());
+		
+		List<Autorizacion> autorizaciones = e.getAutorizacions();
+		
+		for(PersonaAutorizada pa : lpa) {
+			
+			Autorizacion aut = new Autorizacion();
+			aut.setTipo(null); //Revisar
+			
+			AutorizacionPK aut_pk = new AutorizacionPK();
+			aut_pk.setEmpresaId(e.getId());
+			aut_pk.setPersonaAutorizadaId(pa.getId());
+			
+			aut.setEmpresa(e);
+			aut.setId(aut_pk);
+			aut.setPersonaAutorizada(pa);
+			em.persist(aut);
+			autorizaciones.add(aut);
+			
+		}
+		
+		e.setAutorizacions(autorizaciones);
+		}
+
+	@Override
+	public void modificarAutorizados(String idAdm, Cliente c, String id, String identificacion, String nombre, String apellidos, String direccion, Date fecha_nacimiento, String Estado, Date FechaInicio, Date fechaFin) throws UsuarioNoEsAdministrativoException, ClienteNoJuridicoException, PersonaAutorizadaNoExistenteException {
+		
+		Usuario administrador = em.find(Usuario.class, idAdm);
+		
+		if(administrador==null || !administrador.getTipo().equals("A")) { //Si no existe o no es administrativo
+			throw new UsuarioNoEsAdministrativoException();
+		}
+		
+		PersonaAutorizada p = em.find(PersonaAutorizada.class, id);
+		
+		if(p==null) {
+			throw new PersonaAutorizadaNoExistenteException();
+		}
+		
+		if(!c.getEstado().equals("J")) {
+			throw new ClienteNoJuridicoException();
+		}
+		
+		p.setId(id);
+		p.setIdentificacion(identificacion);
+		p.setNombre(nombre);
+		p.setApellidos(apellidos);
+		p.setDireccion(direccion);
+		
+		if(fecha_nacimiento !=null) {
+			p.setFechaNacimiento(fecha_nacimiento);
+		}
+		
+		p.setFechaNacimiento(fecha_nacimiento);
+		
+		if(Estado != null) {
+			p.setEstado(Estado);
+		}
+		
+		if(FechaInicio != null) {
+			p.setFechainicio(FechaInicio);
+		}
+		
+		if(fechaFin != null) {
+			p.setFechafin(fechaFin);
+		}
+		
+	}
 }
--- proyecto/proyecto-ejb/src/main/java/es/uma/proyecto/ejb/GestionCuenta.java
+++ proyecto/proyecto-ejb/src/main/java/es/uma/proyecto/ejb/GestionCuenta.java
@@ -1,10 +1,51 @@
 package es.uma.proyecto.ejb;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.ejb.Local;
+
+import es.uma.proyecto.Cliente;
+import es.uma.proyecto.CuentaReferencia;
+import es.uma.proyecto.DepositaEn;
+import es.uma.proyecto.PersonaAutorizada;
+import es.uma.proyecto.ejb.exceptions.ClienteNoJuridicoException;
+import es.uma.proyecto.ejb.exceptions.PersonaAutorizadaNoExistenteException;
+import es.uma.proyecto.ejb.exceptions.PooledAccountConSolo1CuentaExternaException;
+import es.uma.proyecto.ejb.exceptions.UsuarioNoEsAdministrativoException;
 
 @Local
 public interface GestionCuenta {
 
+	/* Este metodo se encarga de comprobar en primera instancia si existe el administrador que esta intentado abrir la Cuenta,
+	 * seguido de esto. En caso de que el administrador que intenta arbrir la cuenta no exista en la bd, salta una excepcion.
+	 * En otro caso genera la cuenta segregada sin problemas. Junto con la informacion de la cuenta que tiene referenciada.
+	 */
+	
+	public void abrirCuentaFintechSegregada(String idAdm, String iban, String swift, String estado, Date fecha_apertura, Date fecha_cierre, String clasificacion, String comision, CuentaReferencia cr) throws UsuarioNoEsAdministrativoException;
+	
+	/* Este metodo se encarga de comprobar en primera instancia si existe el administrador que esta intentado abrir la Cuenta,
+	 * seguido de esto. En caso de que el administrador que intenta arbrir la cuenta no exista en la bd, salta una excepcion.
+	 * En otro caso genera la cuenta pooled sin problemas. Junto con la informacion de las cuentas que tiene referenciadas.
+	 */
+	
+	public void abrirCuentaFintechPooled(String idAdm, String iban, String swift, String estado, Date fecha_apertura, Date fecha_cierre, String clasificacion, List<DepositaEn> lcr) throws UsuarioNoEsAdministrativoException, PooledAccountConSolo1CuentaExternaException;
+	
+	/* Este metodo se encarga de comprobar en primer lugar que la persona que esta tratando de añadir a una Cuenta cuyo Cliente es una persona juridica 
+	 * sea administrativo. En caso de que no lo sea, salta una excepcion. 
+	 * Lo siguiente a comprobar es que el Cliente que trata de añadir las personas autorizadas sea un cliente juridico. Si no se trata de un cliente juridico salta una excepcion.
+	 * En caso de que no haya saltado ninguna excepcion se asocia correctamente todas estas personas autorizadas a la cuenta Empresa de el cliente en cuestion.
+	 */
+
+	public void anadirAutorizados(String idAdm, List<PersonaAutorizada> lpa, Cliente c) throws UsuarioNoEsAdministrativoException, ClienteNoJuridicoException;
+	
+	/* Este metodo se encarga de comprobar en primer lugar que la persona que esta tratando de modificar una Cuenta cuyo Cliente es una persona juridica
+	 * sea administrativo. En caso de que no lo sea, salta una excepcion.
+	 * Lo siguiente a comprobar es que el Cliente al cual trata de modificarse  la persona autorizada sea un cliente juridico. Si no se trata de un cliente juridico salta una excepcion.
+	 * En caso de que no haya saltado ninguna excepcion se modifican los datos de la persona autorizada en cuestion.
+	 */
+	
+	public void modificarAutorizados(String idAdm, Cliente c, String id, String identificacion, String nombre, String apellidos, String direccion, Date fecha_nacimiento, String Estado, Date FechaInicio, Date fechaFin) throws UsuarioNoEsAdministrativoException, ClienteNoJuridicoException, PersonaAutorizadaNoExistenteException;
 	
 	
 }
