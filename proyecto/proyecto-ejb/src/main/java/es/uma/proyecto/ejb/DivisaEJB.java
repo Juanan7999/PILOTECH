c2dc56fe4b0ba25a2ce590b25db785ce839f3e78
FranLopezCampos <80339518+FranLopezCampos@users.noreply.github.com>
2022-04-26 18:37:03 +0200

    Creacion de cambio de divisa de cliente y cambio de divisa de admin

--- proyecto/proyecto-ejb/src/main/java/es/uma/proyecto/ejb/DivisaEJB.java
+++ proyecto/proyecto-ejb/src/main/java/es/uma/proyecto/ejb/DivisaEJB.java
@@ -1,9 +1,24 @@
 package es.uma.proyecto.ejb;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
+import es.uma.proyecto.Cliente;
+import es.uma.proyecto.Cuenta;
+import es.uma.proyecto.DepositaEn;
+import es.uma.proyecto.Divisa;
+import es.uma.proyecto.PooledAccount;
+import es.uma.proyecto.Usuario;
+import es.uma.proyecto.ejb.exceptions.ClienteBloqueadoException;
+import es.uma.proyecto.ejb.exceptions.ClienteNoExistenteException;
+import es.uma.proyecto.ejb.exceptions.ClienteYaDeBajaException;
+import es.uma.proyecto.ejb.exceptions.CuentasDiferentesException;
+import es.uma.proyecto.ejb.exceptions.DivisaNoExistenteException;
+import es.uma.proyecto.ejb.exceptions.UsuarioEsAdministrativoException;
+import es.uma.proyecto.ejb.exceptions.UsuarioNoEsAdministrativoException;
 
 public class DivisaEJB implements GestionDivisa{
 
@@ -12,6 +27,84 @@ public class DivisaEJB implements GestionDivisa{
 	@PersistenceContext(name="proyecto-ejb")
 	private EntityManager em;
 	
+	
+	@Override
+	public void cambioDeDivisaCliente(String idAdmin,Cliente cliente,PooledAccount cuenta1,PooledAccount cuenta2,Divisa origen,Divisa destino,List<DepositaEn> saldo) throws UsuarioEsAdministrativoException, ClienteNoExistenteException, ClienteBloqueadoException, ClienteYaDeBajaException, CuentasDiferentesException, DivisaNoExistenteException {
+		
+		Usuario administrador = em.find(Usuario.class, idAdmin);
+		
+		if(administrador!=null || administrador.getTipo().equals("A")) {
+			throw new UsuarioEsAdministrativoException();
+		}
+		
+		Cliente clienteEntity = em.find(Cliente.class, cliente);
+		
+		if(clienteEntity == null) {
+			throw new ClienteNoExistenteException();
+		}else if (clienteEntity.getEstado().equals("bloqueado")){
+			throw new ClienteBloqueadoException();
+		}else if (clienteEntity.getEstado().equals("baja")) {
+			throw new ClienteYaDeBajaException();
+		}
+		
+		PooledAccount cuenta1Entity = em.find(PooledAccount.class,cuenta1);
+		
+		PooledAccount cuenta2Entity = em.find(PooledAccount.class, cuenta2);
+		
+		if(cuenta1Entity.getIban() != cuenta2Entity.getIban()) {
+			throw new CuentasDiferentesException();
+		}
+		
+		Divisa origenEntity = em.find(Divisa.class, origen);
+		Divisa destinoEntity = em.find(Divisa.class, destino);
+		
+		if(origenEntity == null) {
+			throw new DivisaNoExistenteException();
+		}
+		
+		if(destinoEntity == null) {
+			throw new DivisaNoExistenteException();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	@Override
+	public void cambioDeDivisaAdmin(String idAdmin,PooledAccount cuenta1,PooledAccount cuenta2,Divisa origen,Divisa destino,List<DepositaEn> saldo) throws UsuarioNoEsAdministrativoException, CuentasDiferentesException, DivisaNoExistenteException {
+		
+		Usuario administrador = em.find(Usuario.class, idAdmin);
+		
+		if(administrador == null || !administrador.getTipo().equals("A")) {
+			throw new UsuarioNoEsAdministrativoException();
+		}
+		
+		PooledAccount cuenta1Entity = em.find(PooledAccount.class,cuenta1);
+		
+		PooledAccount cuenta2Entity = em.find(PooledAccount.class, cuenta2);
+		
+		if(cuenta1Entity.getIban() != cuenta2Entity.getIban()) {
+			throw new CuentasDiferentesException();
+		}
+		
+		Divisa origenEntity = em.find(Divisa.class, origen);
+		Divisa destinoEntity = em.find(Divisa.class, destino);
+		
+		if(origenEntity == null) {
+			throw new DivisaNoExistenteException();
+		}
+		
+		if(destinoEntity == null) {
+			throw new DivisaNoExistenteException();
+		}
+		
+		
+	}
 	
 	
 }
--- proyecto/proyecto-ejb/src/main/java/es/uma/proyecto/ejb/GestionDivisa.java
+++ proyecto/proyecto-ejb/src/main/java/es/uma/proyecto/ejb/GestionDivisa.java
@@ -2,9 +2,36 @@ package es.uma.proyecto.ejb;
 
 import javax.ejb.Local;
 
+import es.uma.proyecto.Cliente;
+import es.uma.proyecto.Cuenta;
+import es.uma.proyecto.Divisa;
+import es.uma.proyecto.PooledAccount;
+import es.uma.proyecto.ejb.exceptions.ClienteBloqueadoException;
+import es.uma.proyecto.ejb.exceptions.ClienteNoExistenteException;
+import es.uma.proyecto.ejb.exceptions.ClienteYaDeBajaException;
+import es.uma.proyecto.ejb.exceptions.CuentasDiferentesException;
+import es.uma.proyecto.ejb.exceptions.UsuarioEsAdministrativoException;
+import es.uma.proyecto.ejb.exceptions.UsuarioNoEsAdministrativoException;
+
 @Local
 public interface GestionDivisa {
 
+	//La aplicacion permitira a un cliente/autorizado realizar un cambio de divisas en una cuenta agrupada (pooled)
+	//El origen y el destino es la misma cuenta. La cuenta tiene que tener saldo en las divisas de origen y destino
+	//Para poder realizar un cambio de divisas será necesario que la cuenta tenga saldos en las divisas de origen y destino. 
+	//Los saldos de las cuentas asociadas con la cuenta agrupada deberá actualizarse también
+	//No sera posible realizar un cambio de divisa en cuentas segregadas
+	
+	public void cambioDeDivisaCliente(String idAdmin,Cliente cliente,PooledAccount cuenta1,PooledAccount cuenta2,Divisa origen,Divisa destino) throws UsuarioEsAdministrativoException, ClienteNoExistenteException, ClienteYaDeBajaException, ClienteBloqueadoException, CuentasDiferentesException;
+	
+	
+	//La aplicacion permitira a un cliente/autorizado realizar un cambio de divisas en una cuenta agrupada (pooled)
+	//El origen y el destino es la misma cuenta. La cuenta tiene que tener saldo en las divisas de origen y destino
+	//Para poder realizar un cambio de divisas será necesario que la cuenta tenga saldos en las divisas de origen y destino. 
+	//Los saldos de las cuentas asociadas con la cuenta agrupada deberá actualizarse también
+	//No sera posible realizar un cambio de divisa en cuentas segregadas
+	
+	public void cambioDeDivisaAdmin(String idAdmin,PooledAccount cuenta1,PooledAccount cuenta2,Divisa origen,Divisa destino) throws UsuarioNoEsAdministrativoException, CuentasDiferentesException;
 	
 	
 }
