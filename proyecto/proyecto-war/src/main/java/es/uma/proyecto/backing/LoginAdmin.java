e36abc37862b4cf81d4803e8e692bdb288d6c687
FranLopezCampos <80339518+FranLopezCampos@users.noreply.github.com>
2022-05-16 18:56:54 +0200

    LoginAdmin backing

---
+++ proyecto/proyecto-war/src/main/java/es/uma/proyecto/backing/LoginAdmin.java
@@ -0,0 +1,58 @@
+package es.uma.proyecto.backing;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import es.uma.proyecto.Usuario;
+import es.uma.proyecto.ejb.UsuarioEJB;
+import es.uma.proyecto.ejb.exceptions.ContraseñaIncorrectaException;
+import es.uma.proyecto.ejb.exceptions.UsuarioNoEncontradoException;
+import es.uma.proyecto.ejb.exceptions.UsuarioNoEsAdministrativoException;
+
+@Named(value = "loginAdmin")
+@RequestScoped
+public class LoginAdmin {
+
+	
+	@Inject
+	private UsuarioEJB usuarioejb;
+	
+	private Usuario usuario;
+	
+	public LoginAdmin() {
+		usuario = new Usuario();
+	}
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	public String loginAdmin() {
+		
+		try {
+			usuarioejb.LoginAdmin(usuario.getNombreUsuario(), usuario.getPassword(), usuario.getTipo());
+		}catch(UsuarioNoEncontradoException e) {
+			FacesMessage fm = new FacesMessage("La cuenta no existe");
+			FacesContext.getCurrentInstance().addMessage("loginAdmin:user", fm);
+		} catch (ContraseñaIncorrectaException e) {
+			FacesMessage fm = new FacesMessage("La password no es valida");
+			FacesContext.getCurrentInstance().addMessage("loginAdmin:pass", fm);
+		} catch (UsuarioNoEsAdministrativoException e) {
+			FacesMessage fm = new FacesMessage("El usuario no es administrativo");
+			FacesContext.getCurrentInstance().addMessage("loginAdmin:tipo", fm);
+		}
+		
+		
+		return "paginaprincipal.xhtml";
+	}
+	
+	
+	
+}
