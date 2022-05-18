f9693b49bc2fa718d5ed924674eb754a3e82a472
oussamabe07 <oussamabe@alu.uma.es>
2022-05-18 17:53:46 +0200

    Update BaseDatos.java

--- proyecto/proyecto-ejb/src/test/java/es/uma/proyecto/ejb/test/BaseDatos.java
+++ proyecto/proyecto-ejb/src/test/java/es/uma/proyecto/ejb/test/BaseDatos.java
@@ -19,7 +19,7 @@ import es.uma.proyecto.Segregada;
 import es.uma.proyecto.Transaccion;
 import es.uma.proyecto.Usuario;
 
-public class BaseDatos {d
+public class BaseDatos {
 	public static void inicializaBaseDatos(String nombreUnidadPersistencia) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory(nombreUnidadPersistencia);
 		EntityManager em = emf.createEntityManager();
