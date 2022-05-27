824219276bb8fb4dde781a0aa707feea3f2391b8
FranLopezCampos <80339518+FranLopezCampos@users.noreply.github.com>
2022-05-27 19:19:01 +0200

    Mostrar Transacciones

---
+++ proyecto/proyecto-war/src/main/java/es/uma/proyecto/backing/MostrarTransacciones.java
@@ -0,0 +1,84 @@
+package es.uma.proyecto.backing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import es.uma.proyecto.Cuenta;
+import es.uma.proyecto.CuentaFintech;
+import es.uma.proyecto.Individual;
+import es.uma.proyecto.Transaccion;
+import es.uma.proyecto.ejb.GestionCuenta;
+import es.uma.proyecto.ejb.GestionUsuario;
+import es.uma.proyecto.ejb.exceptions.CuentaNoExistenteException;
+
+
+@Named(value = "mostrarTransacciones")
+@ViewScoped
+public class MostrarTransacciones {
+	
+	@Inject
+	private GestionCuenta cuentaEJB;
+	
+	@Inject
+	private InfoSesion sesion;
+	
+	@Inject
+	private GestionUsuario usuarioEJB;
+
+	private List<Transaccion> transaccionesOrigen;
+	
+	private List<Transaccion> transaccionesDestino;
+	
+	private CuentaFintech cuenta;
+	
+	
+	public MostrarTransacciones() {
+		
+		transaccionesOrigen = new ArrayList<>();
+		transaccionesDestino = new ArrayList<>();
+		cuenta = new CuentaFintech();
+		
+	}
+	
+	public CuentaFintech getCuenta() {
+		return cuenta;
+	}
+	
+	public void setCuenta(CuentaFintech cuenta) {
+		this.cuenta = cuenta;
+	}
+	
+	public List<Transaccion> getTransaccionesOrigen() throws CuentaNoExistenteException{
+		
+		return cuentaEJB.getTransaccionesSonOrigen(cuenta.getIban());
+			
+		
+	}
+	
+	public void setTransaccionesOrigen(List<Transaccion> transaccionesOrigen){
+		this.transaccionesOrigen = transaccionesOrigen;
+	}
+	
+	
+	public List<Transaccion> getTransaccionesDestino() throws CuentaNoExistenteException{
+		
+		return cuentaEJB.getTransaccionesSonDestino(cuenta.getIban());
+		
+	}
+	
+	public void setTransaccionesDestino(List<Transaccion> transaccionesDestino){
+		this.transaccionesDestino = transaccionesDestino;
+	}
+	
+	public String accion(String c) throws CuentaNoExistenteException {
+		
+		this.cuenta = cuentaEJB.devolverCuenta(c);
+		
+		return "paginaTransacciones.xhtml";
+	}
+	
+}
